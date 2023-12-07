16c5b2adaf09f8dea8b1a1b1561c648a3ed62a8f
Morozov Nikita <nikitwww@gmail.com>
2023-12-07 15:02:51 +0300

    unnamed

--- first_sem/programming/homeworks/HW_8/8_4/main.c
+++ first_sem/programming/homeworks/HW_8/8_4/main.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdarg.h>
 
-int func() {
-      
+double func(int n, ...){
+    double sum = 0;
+    int k = 0;
+    va_list params;
+    va_start(params, n);
+    for(int i=0;i<n; i++){
+        int m = va_arg(params, int);
+        k+=m;
+        for (int i = 0; i < m; i++)
+            sum += va_arg(params, double);
+    }
+    va_end(params);
+    return sum/(double) k;
 }
 
 int main(void){
-    int n;
-    scanf("%d", &n);
-    printf("%d\n", n);
+    printf("%lf\n", func(2, 3, 1231.141, 12414.2323, 60.5, 2, 23.9, 1.000001));
 }
